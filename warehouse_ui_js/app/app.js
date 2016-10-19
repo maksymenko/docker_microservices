a8063d690ebbe74b5d7be9b0b8073a8e1320f047
sergii <sergey.maksymenko@gmail.com>
2016-10-19 01:11:56 -0700

    angular ui routes added

--- warehouse_ui_js/app/app.js
+++ warehouse_ui_js/app/app.js
@@ -1,2 +1,26 @@
-var str = 'test';
-console.log("my str : " + str);
+'use strict';
+
+angular.module('warehouse', [
+  'StoreModule',
+  'ui.router'
+]).
+config(function($stateProvider, $urlRouterProvider) {
+  var storeState = {
+    name: 'store',
+    url: '/store',
+    controller: 'StoreController',
+    templateUrl: 'store.html'
+  }
+
+  var settingsState = {
+    name: 'settings',
+    url: '/settings',
+    template: '<h3>Settings</h3>'
+  }
+
+  // For any unmatched url, send to /route1
+//  $urlRouterProvider.otherwise("/store")
+  $stateProvider.state(storeState);
+  $stateProvider.state(settingsState);
+
+});
--- warehouse_ui_js/app/store-controller.js
+++ warehouse_ui_js/app/store-controller.js
@@ -1,5 +1,5 @@
-angular.module('warehouse', [])
-    .controller('ItemController', function($scope, $http) {
+angular.module('StoreModule', [])
+    .controller('StoreController', function($scope, $http) {
         $http.get('http://localhost:8080/item').
             then(function(response) {
                 $scope.itemList = response.data;
