74092d28481c26a8f56cf2cd04264d0d43bc046e
sergii <sergey.maksymenko@gmail.com>
2016-12-29 01:34:54 -0800

    ngResource configured

--- warehouse_ui_js/src/app/catalog/catalog.service.js
+++ warehouse_ui_js/src/app/catalog/catalog.service.js
@@ -6,8 +6,16 @@ class CatalogService {
   }
 
   loadCatalog() {
-    let itemsResource = this._$resource('http://127.0.0.1:8081/item');
-    return itemsResource.query();
+    let itemsResource = this._$resource(
+      'http://127.0.0.1:8081/item',
+      {},
+      {
+        get: {
+          method: 'GET',
+          isArray: true
+        }
+      });
+    return itemsResource.get();
   }
 }
 
