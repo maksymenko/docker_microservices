4cadfbba4bd753e397e4bae090fae5e0523f5589
sergii <sergey.maksymenko@gmail.com>
2017-04-26 22:03:59 -0700

    added test dependency to controller

--- web_client/src/app/about/about.controller.test.js
+++ web_client/src/app/about/about.controller.test.js
@@ -1,10 +1,12 @@
+import AboutController from './about.controller';
+
 describe("Suite for about controller", function() {
     it("test if author returns valid value", function() {
         expect(true).toBeTruthy();
     });
 
     it("test  author value", function() {
-        let author = 'First Last Name';
-        expect(author).toEqual('First Last Name');
+        let ctrl = new AboutController();
+        expect(ctrl.getAuthor()).toEqual('First Last Name');
     });
 });
